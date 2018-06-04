c53084254f875cb0983683c2ef0182e003993471
wyxiu <455207756@qq.com>
2018-06-04 20:43:40 +0800

    1212

---
+++ JSText/day21/php学习.php
@@ -0,0 +1,11 @@
+<?php
+	echo "<meta charset='utf-8'>";
+		
+		$arr = array("张三","李四","王五");
+		
+	echo"<br>第一个人的姓名是：".$arr[0];
+		
+		$arr1 = array("pro1"=>"语文","pro2"=>"数学");
+		
+	echo"<br>喜欢的学科是：".$arr1["pro1"];
+?>
