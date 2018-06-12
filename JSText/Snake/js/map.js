7e03159de40168ffef70f4444dbcec5244901269
wyxiu <455207756@qq.com>
2018-06-12 23:43:19 +0800

    fgf

--- JSText/Snake/js/map.js
+++ JSText/Snake/js/map.js
@@ -1,49 +1,39 @@
-
 //赋值表示初始化赋值
-function Map({width =600,height=600,background ="#ccc"}={}){
+function Map({
+	width = 600,
+	height = 600,
+	background = "#ccc"
+} = {}) {
 	this.width = width;
 	this.height = height;
 	this.background = background;
-	this.dom = null;//就相当于css中的div，这里用js动态创建
+	this.dom = null; //就相当于css中的div，这里用js动态创建
 	this.createDom();
 }
 Map.prototype = {
-	constructor : Map,
+	constructor: Map,
 	//设置属性
-	createDom :　function(){
+	createDom: 　 function() {
 		this.dom = document.createElement("div");
-		Tools.css(this.dom,{
-			width : this.width + "px",
-			height : this.height +"px",
-			background:this.background,
-			position : "relative",
-			margin : "10px auto"
+		Tools.css(this.dom, {
+			width: this.width + "px",
+			height: this.height + "px",
+			background: this.background,
+			position: "relative",
+			margin: "10px auto"
 		});
 		//把map添加到body中显示
-		document.body.appendChild(this.dom);
-	},	
-		//添加角色在map中
-		addRole:function(role){
-			this.dom.appendChild(role);
+		
+		var children = document.body.children;
+		for(var i=0;i<children.length;i++){
+			if(children[i].nodeName === "DIV"){
+				document.body.removeChild(children[i]);
+			}
 		}
+		document.body.appendChild(this.dom);
+	},
+	//添加角色在map中
+	addRole: function(role) {
+		this.dom.appendChild(role);
+	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- JSText/Snake/js/snake.js
+++ JSText/Snake/js/snake.js
@@ -1,82 +1,132 @@
-
 //定义方向
 const Direction = {
-	LEFT :Symbol(),
-	RIGHT :Symbol(),
-	UP :Symbol(),
-	DOWN :Symbol()
+	LEFT: Symbol(),
+	RIGHT: Symbol(),
+	UP: Symbol(),
+	DOWN: Symbol()
 }
 
 class Snake {
-	constructor({body=[],width = 20,height=20,direction = Direction.RIGHT}={}) {
-	    this.body = body;
-	    this.width = width;
-	    this.height = height;
-	    this.direction = direction;
-	    this.map = null;//关联的地图
-	    this.init();//初始化
+	constructor({
+		body = [],
+		width = 20,
+		height = 20,
+		direction = Direction.RIGHT
+	} = {}) {
+		this.body = body;
+		this.width = width;
+		this.height = height;
+		this.direction = direction;
+		this.map = null; //关联的地图
+		this.init(); //初始化
+
 	}
-	
+
 	//初始化蛇身体
-	init(){
-		if(this.body.length === 0){
-			this.body = [
-					{x:2,y:2,dom:null,background : Tools.randomColor()},
-					{x:3,y:2,dom:null,background : Tools.randomColor()},
-					{x:4,y:2,dom:null,background : Tools.randomColor()},
-					{x:5,y:2,dom:null,background : Tools.randomColor()},
-					{x:6,y:2,dom:null,background : Tools.randomColor()},
-					{x:7,y:2,dom:null,background : "#f00"}
-			];
+	init() {
+		if (this.body.length === 0) {
+			this.body = [{
+				x: 2,
+				y: 2,
+				dom: null,
+				background: Tools.randomColor()
+			}, {
+				x: 3,
+				y: 2,
+				dom: null,
+				background: Tools.randomColor()
+			}, {
+				x: 4,
+				y: 2,
+				dom: null,
+				background: Tools.randomColor()
+			}, {
+				x: 5,
+				y: 2,
+				dom: null,
+				background: Tools.randomColor()
+			}, {
+				x: 6,
+				y: 2,
+				dom: null,
+				background: Tools.randomColor()
+			}, {
+				x: 7,
+				y: 2,
+				dom: null,
+				background: "#f00"
+			}];
 		}
 	}
-	
-	
+
 	//创建对象
-	createDom(){
-		this.body.forEach(bd) => {
-			if(bd.dom ===null ){
+	createDom() {
+		this.body.forEach((bd) => {
+			if (bd.dom === null) {
 				bd.dom = document.createElement("div");
-				Tools.css(bd.dom,{
-					width : this.width +"px",
-					height :this.height +"px",
-					position :"absolute",
-					background:bd.background
+				Tools.css(bd.dom, {
+					width: this.width + "px",
+					height: this.height + "px",
+					position: "absolute",
+					background: bd.background
 				});
-				
+
 				this.map.addRole(bd.dom);
-				
+
 			}
-			Tools.css(bd.dom,{
-				left: bd.x* this.width + "px",
-				top: bd.y* this.height +"px"
+			Tools.css(bd.dom, {
+				left: bd.x * this.width + "px",
+				top: bd.y * this.height + "px"
 			});
-			
+
 		});
 	}
-	
-	move (){
+
+	move() {
 		let len = this.body.length;
 		//移动身体
-		for(let i=0;i<len - 1;i++){
-			this.body[i].x = this.body[i+1].x;
-			this.body[i].y = this.body[i+1].y;
+		for (let i = 0; i < len - 1; i++) {
+			this.body[i].x = this.body[i + 1].x;
+			this.body[i].y = this.body[i + 1].y;
 		}
 		//移动脑袋
-		if(this.direction = Direction.RIGHT)
-				this.bodies[len - 1].x++;
-		else if(this.direction = Direction.LEFT)
-				this.bodies[len - 1].x--;
-		else if(this.direction = Direction.up)
-				this.bodies[len - 1].y--;
-		else if(this.direction = Direction.DOWN)
-				this.bodies[len - 1].y++;
+		if (this.direction == Direction.RIGHT) {
+			this.body[len - 1].x++;
+		} else if (this.direction == Direction.LEFT) {
+			this.body[len - 1].x--;
+		} else if (this.direction == Direction.up) {
+			this.body[len - 1].y--;
+		} else if (this.direction == Direction.DOWN) {
+			this.body[len - 1].y++;
+		}
+
+		console.log(this.body[len - 1].x);
+		if (this.body[len - 1].x < 0 || this.body[len - 1].x >= this.map.width / this.width) {
+			return true;
+		} else if (this.body[len - 1].y < 0 || this.body[len - 1].y >= this.map.height / this.height) {
+			return true;
+		}
+
+		for (var i = 0; i < len - 1; i++) {
+			if (this.body[len - 1].x === this.body[i].x && this.body[len - 1].y === this.body[i].y) {
+				return true;
+			}
+		}
+		this.createDom();
+		return false;
 	}
-	
-	setDirection(code){
-		if (code === 37)
-			this.direction = Direction.LEFT;
-		else if (code === 38)
+	endGame() {
+		alert("撞死了");
+	}
+
+	setDirection(code) {
+		console.log("----" + code);
+
+		if (code === 37) {
+			if (this.direction != Direction.RIGHT) {
+				this.direction = Direction.LEFT;
+			}
+		} else if (code === 38)
 			this.direction = Direction.UP;
 		else if (code === 39)
 			this.direction = Direction.RIGHT;
@@ -84,20 +134,3 @@ class Snake {
 			this.direction = Direction.DOWN;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
