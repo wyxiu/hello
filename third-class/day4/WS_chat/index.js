89ca4f01db77c233c47ed417b36d59d7f0d65539
wyxiu <455207756@qq.com>
2018-07-05 23:30:34 +0800

    12121

--- third-class/day4/WS_chat/index.js
+++ third-class/day4/WS_chat/index.js
@@ -1,45 +1,15 @@
-const WebSocket =require('ws');
-const wss = new WebSocket.Server({port:8080});
-
-wss.on('connection',(ws)=>{
-	ws.on('message',(message)=>{
-		console.log('received:%s',message);
-		wss.clients.forEach((client)=>{
-		client.send(message);
-	});
-	
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+	port: 8080
 });
+
+wss.on('connection', (ws) => {
+	ws.on('message', (message) => {
+		console.log('received:%s', message);
+		wss.clients.forEach((client) => {
+			client.send(message);
+		});
+
+	});
 	ws.send('something');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- third-class/day4/chat/index.js
+++ third-class/day4/chat/index.js
@@ -6,84 +6,35 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //处理一些客户端的请求，就是路由：当以get请求方式对"/"资源请求时的处理
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	//向客户端发送文本字符串数据
-  //res.send('<h1>Hello world</h1>');
-  res.sendFile(__dirname + '/index.html');
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname + '/index.html');
 });
 
 //当socket服务器监听到客户端连接，会触发connection事件
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+	//console.log('a user connected');
+	  var address = socket.handshake; 
+	  console.log("New connection from " + address.address + ":" + address.port);
+	socket.join('group');
+	socket.broadcast.to('group').emit('broad', socket.handshake.address+" joined");
 	//绑定chat message事件
-	socket.on('chat message', function(msg){
-    //console.log('message: ' + msg);
-     io.emit('broad', msg);
-  });
-	
+	socket.on('chat message', function(msg) {
+		console.log('message: ' + msg);
+		//io.emit('broad', "server:" + msg);
+		socket.broadcast.to('group').emit('broad', socket.id+":"+msg);
+	});
+	socket.on('disconnecting', function(msg) {
+		//let rooms = Object.keys(socket.rooms);
+		console.log('disconnecting: '+Object.keys(socket.rooms));
+		socket.broadcast.to('group').emit('broad', socket.id+" leaved");
+	});
+
 });
-
-  
-
 
 //服务器在3000端口监听客户端的请求
-http.listen(3000, function(){
+http.listen(3000, function() {
 	//在控制台上打印监听成功消息
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
