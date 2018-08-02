35f05beeaa2127da528da7e7be372ee6cfe0cc08
wyxiu <455207756@qq.com>
2018-08-02 16:55:45 +0800

    'fff'

--- third-class/React/Boke/src/components/logo/Logo.js
+++ third-class/React/Boke/src/components/logo/Logo.js
@@ -5,7 +5,7 @@ export default class Logo extends Component {
  
   render() {
     return (
-        <div className="logo-bar">
+      <div className="logo-bar tada">
           <img className="logo" src={logo} alt="myBlog" />
           <h3>myBlog</h3>
       </div>
--- third-class/React/Boke/src/pages/Detail.js
+++ third-class/React/Boke/src/pages/Detail.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import { withRouter } from 'react-router-dom'
+import Loading from '../components/loading/Loading';
 class Detail extends Component {
     constructor(){
         super();
         this.state={
-            text:[
-                {id:1,context:"hsdfjhjkasdhf"},
-                { id: 2, context: "点击附件奥斯" },
-                { id: 3, context: "驾驶的咖啡机" }
-            ]
+            text:[ ],
+            isLoading:true
         }
+    }
+
+    componentDidMount(){
+        axios.get(`http://jsonplaceholder.typicode.com/posts/${this.props.match.params.id}`).then(resp => {
+            // console.log(resp)
+            if (resp.status === 200) {
+                this.setState({
+                    text: resp.data,
+                    isLoading: false
+                })
+            }
+        });
     }
     render() {
         return (
-            <div>
-                {this.props.match.params.id}
+           <div>    
+            {
+                this.state.isLoading
+                ?
+                <Loading/>
+                :
+               <div>
+                   <h2>{this.state.text.title}</h2>
+                 <p>{this.state.text.body}</p>
+               </div>
+            }
             </div>
+            
         )
     }
 }
